7592e8883d8adad73fd99640dd4da9c8b30c1c1e
mijHwang <102636178+mijHwang@users.noreply.github.com>
2025-10-03 13:47:10 -0300

    Entity Manager Factory injection

--- src/main/java/ar/edu/utn/dds/k3003/worker/WorkerStarter.java
+++ src/main/java/ar/edu/utn/dds/k3003/worker/WorkerStarter.java
@@ -4,41 +4,57 @@ import com.rabbitmq.client.*;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 @Component
 public class WorkerStarter {
 
+    private static final Logger log = Logger.getLogger(String.valueOf(WorkerStarter.class));
+
     private Connection connection;
+
+    @Autowired
+    private EntityManagerFactory emf;
 
     @PostConstruct
     public void startWorker() throws Exception {
-        System.out.println("Starting worker inside Spring Boot...");
+        log.info("Starting worker");
 
-        Map<String, String> env = System.getenv();
-        ConnectionFactory factory = new ConnectionFactory();
+        try {
 
-        // Safer: use CLOUDAMQP_URL if provided
-        if (env.containsKey("CLOUDAMQP_URL")) {
-            factory.setUri(env.get("CLOUDAMQP_URL"));
-        } else {
-            factory.setHost(env.get("QUEUE_HOST"));
-            factory.setUsername(env.get("QUEUE_USERNAME"));
-            factory.setPassword(env.get("QUEUE_PASSWORD"));
-            factory.setVirtualHost(env.getOrDefault("QUEUE_USERNAME", "/"));
+
+            Map<String, String> env = System.getenv();
+            ConnectionFactory factory = new ConnectionFactory();
+
+            // Safer: use CLOUDAMQP_URL if provided
+            if (env.containsKey("CLOUDAMQP_URL")) {
+                factory.setUri(env.get("CLOUDAMQP_URL"));
+            } else {
+                factory.setHost(env.get("QUEUE_HOST"));
+                factory.setUsername(env.get("QUEUE_USERNAME"));
+                factory.setPassword(env.get("QUEUE_PASSWORD"));
+                factory.setVirtualHost(env.getOrDefault("QUEUE_USERNAME", "/"));
+            }
+
+            factory.setAutomaticRecoveryEnabled(true);
+
+
+            this.connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            hechoWorker worker = new hechoWorker(channel, env.get("QUEUE_NAME"), emf);
+            worker.init();
+
+            log.info("Worker initialized and consuming messages!");
+        }catch (Exception e) {
+
+            log.info("Failed to start worker");
         }
 
-        this.connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("yourPU");
-        hechoWorker worker = new hechoWorker(channel, env.get("QUEUE_NAME"), emf);
-        worker.init();
-
-        System.out.println("Worker initialized and consuming messages!");
     }
 
     @PreDestroy
